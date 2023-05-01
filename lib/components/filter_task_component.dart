bfcb70c842a9edc5b8e82971c50ac4097cd86cba
Gustavo Salles <87676513+salles97@users.noreply.github.com>
2023-05-01 00:21:40 -0300

    feat: update interface of filter's tasks to Card

--- lib/components/filter_task_component.dart
+++ lib/components/filter_task_component.dart
@@ -16,104 +16,108 @@ class _FilterTasks extends State<FilterTasks> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: 40,
-      decoration: const BoxDecoration(
-        color: Color.fromRGBO(217, 217, 217, 1),
-        // color: Colors.transparent
-      ),
-      margin: const EdgeInsets.only(left: 0, right: 0),
-      child: Row(
-        children: [
-          const Spacer(flex: 1),
-          ElevatedButton(
-            style: ButtonStyle(
-              backgroundColor: _hasFavoriteBeenPressed
-                  ? const MaterialStatePropertyAll(
-                      Color.fromRGBO(217, 217, 217, 1),
-                    )
-                  : MaterialStatePropertyAll(
-                      primaryColor,
-                    ),
-              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0), // raio das bordas
+    return Card(
+      margin: const EdgeInsets.only(left: 0, right: 0, top: 0),
+      elevation: 10,
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 40,
+        decoration: const BoxDecoration(
+          color: Colors.transparent,
+        ),
+        child: Row(
+          children: [
+            const Spacer(flex: 1),
+            ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: _hasFavoriteBeenPressed
+                    ? const MaterialStatePropertyAll(
+                        Color.fromRGBO(217, 217, 217, 1),
+                      )
+                    : MaterialStatePropertyAll(
+                        primaryColor,
+                      ),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius:
+                        BorderRadius.circular(10.0), // raio das bordas
+                  ),
+                ),
+                minimumSize: MaterialStateProperty.all<Size>(
+                  Size(10, 22), // largura e altura mínimas
                 ),
               ),
-              minimumSize: MaterialStateProperty.all<Size>(
-                Size(10, 22), // largura e altura mínimas
-              ),
+              onPressed: () => {
+                setState(
+                  () {
+                    _hasFavoriteBeenPressed = !_hasFavoriteBeenPressed;
+                  },
+                ),
+              },
+              child: _hasFavoriteBeenPressed
+                  ? (const Text('FINALIZADOS',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 10,
+                          fontWeight: FontWeight.bold)))
+                  : (const Text('FINALIZADOS',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 10,
+                          fontWeight: FontWeight.bold))),
             ),
-            onPressed: () => {
-              setState(
-                () {
-                  _hasFavoriteBeenPressed = !_hasFavoriteBeenPressed;
-                },
+            const Spacer(flex: 1),
+            ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: _hasPendentsBeenPressed
+                    ? const MaterialStatePropertyAll(
+                        Color.fromRGBO(217, 217, 217, 1),
+                      )
+                    : MaterialStatePropertyAll(
+                        primaryColor,
+                      ),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius:
+                        BorderRadius.circular(10.0), // raio das bordas
+                  ),
+                ),
+                minimumSize: MaterialStateProperty.all<Size>(
+                  Size(10, 22), // largura e altura mínimas
+                ),
               ),
-            },
-            child: _hasFavoriteBeenPressed
-                ? (const Text('FINALIZADOS',
-                    style: TextStyle(
+              onPressed: () => {
+                setState(
+                  () {
+                    _hasPendentsBeenPressed = !_hasPendentsBeenPressed;
+                  },
+                ),
+              },
+              child: _hasPendentsBeenPressed
+                  ? (const Text('PENDENTES',
+                      style: TextStyle(
                         color: Colors.black,
                         fontSize: 10,
-                        fontWeight: FontWeight.bold)))
-                : (const Text('FINALIZADOS',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 10,
-                        fontWeight: FontWeight.bold))),
-          ),
-          const Spacer(flex: 1),
-          ElevatedButton(
-            style: ButtonStyle(
-              backgroundColor: _hasPendentsBeenPressed
-                  ? const MaterialStatePropertyAll(
-                      Color.fromRGBO(217, 217, 217, 1),
-                    )
-                  : MaterialStatePropertyAll(
-                      primaryColor,
-                    ),
-              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0), // raio das bordas
-                ),
-              ),
-              minimumSize: MaterialStateProperty.all<Size>(
-                Size(10, 22), // largura e altura mínimas
-              ),
+                        fontWeight: FontWeight.bold,
+                      )))
+                  : (const Text('PENDENTES',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 10,
+                          fontWeight: FontWeight.bold))),
             ),
-            onPressed: () => {
-              setState(
-                () {
-                  _hasPendentsBeenPressed = !_hasPendentsBeenPressed;
-                },
-              ),
-            },
-            child: _hasPendentsBeenPressed
-                ? (const Text('PENDENTES',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 10,
-                      fontWeight: FontWeight.bold,
-                    )))
-                : (const Text('PENDENTES',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 10,
-                        fontWeight: FontWeight.bold))),
-          ),
-          const Spacer(flex: 7),
-          Switch(
-            value: isSwitched,
-            onChanged: (value) {
-              setState(() {
-                isSwitched = value;
-              });
-            },
-          ),
-          const Spacer(flex: 1),
-        ],
+            const Spacer(flex: 7),
+            Switch(
+              value: isSwitched,
+              onChanged: (value) {
+                setState(() {
+                  isSwitched = value;
+                });
+              },
+            ),
+            const Spacer(flex: 1),
+          ],
+        ),
       ),
     );
   }
