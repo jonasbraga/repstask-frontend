f0060a3901675ef57bdbba53b90f02bfc9ea1636
cheycattani <cheyennecattani@hotmail.com>
2023-06-05 16:24:13 -0300

    escrita

--- lib/controllers/cadastro_controller.dart
+++ lib/controllers/cadastro_controller.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 import 'package:reptask/configs/config.dart';
 import 'package:reptask/models/cadastro_model.dart';
 
-class LoginController {
+class CadastroController {
   Future createLogin(CadastroModel newCadastro) async {
     final Uri uri = Uri.parse('http://$backendAdress/cadastro');
     final headers = {HttpHeaders.contentTypeHeader: 'application/json'};
